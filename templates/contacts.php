0ec17ca22f2d838766a87b4d4cb6cf8091b9967d
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-12-28 18:48:54 +0300

    upd

---
+++ templates/contacts.php
@@ -0,0 +1,36 @@
+<?php
+/*
+Template Name: Контакты
+*/
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> itemscope itemtype="http://schema.org/WebSite">
+
+<head>
+  <?php get_template_part('partials/head'); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php wp_body_open(); ?>
+
+  <div class="flex flex-col min-h-screen">
+    <?php get_template_part('partials/header'); ?>
+
+    <div class="page-section">
+      <div class="container">
+        <h1 class="page-section__title">
+          <?php the_title(); ?>
+        </h1>
+        <div class="page-section__content">
+          <?php the_content(); ?>
+        </div>
+        <?php get_template_part('partials/legal'); ?>
+        <?php get_template_part('partials/feedback'); ?>
+      </div>
+    </div>
+
+    <?php get_template_part('partials/footer'); ?>
+  </div>
+</body>
+
+</html>
