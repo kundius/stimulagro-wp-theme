b8db07abfdc6a2c6f67b1020e8578ce6b6c6a07c
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-12-29 08:47:22 +0300

    upd

--- templates/certificates.php
+++ templates/certificates.php
@@ -31,28 +31,26 @@ $certificates_query = new WP_Query([
         </div>
 
         <?php if ($certificates_query->have_posts()): ?>
-        <section class="certificates">
-          <div class="certificates-list">
-            <?php while ($certificates_query->have_posts()): ?>
-            <?php $certificates_query->the_post(); ?>
-            <article class="certificates-card">
-              <?php if ($thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'full')): ?>
-              <div class="certificates-card__image">
-                <img src="<?php echo $thumbnail; ?>" />
-              </div>
-              <a href="<?php echo $thumbnail; ?>" class="certificates-card__link" data-fslightbox="gallery" target="_blank">
-                <span class="certificates-card__link-icon">
-                  <span class="icon icon-search"></span>
-                </span>
-              </a>
-              <?php endif; ?>
-              <div class="certificates-card__title"><?php echo esc_html(get_the_title()); ?></div>
-            </article>
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
-          </div>
-          <?php echo get_pagination($certificates_query); ?>
-        </section>
+        <div class="certificates-list">
+          <?php while ($certificates_query->have_posts()): ?>
+          <?php $certificates_query->the_post(); ?>
+          <article class="certificates-card">
+            <?php if ($thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'full')): ?>
+            <div class="certificates-card__image">
+              <img src="<?php echo $thumbnail; ?>" />
+            </div>
+            <a href="<?php echo $thumbnail; ?>" class="certificates-card__link" data-fslightbox="gallery" target="_blank">
+              <span class="certificates-card__link-icon">
+                <span class="icon icon-search"></span>
+              </span>
+            </a>
+            <?php endif; ?>
+            <div class="certificates-card__title"><?php echo esc_html(get_the_title()); ?></div>
+          </article>
+          <?php endwhile; ?>
+          <?php wp_reset_postdata(); ?>
+        </div>
+        <?php echo get_pagination($certificates_query); ?>
         <?php endif; ?>
       </div>
     </div>
