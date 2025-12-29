4f06811574cd09a6e5c1d5a05eb920ba6104ae1f
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-12-29 09:19:35 +0300

    upd

--- templates/certificates.php
+++ templates/certificates.php
@@ -5,8 +5,7 @@ Template Name: Сертификаты
 $certificates_query = new WP_Query([
   'post_type' => 'certificate',
   'posts_per_page' => 4,
-]);
-?>
+]); ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> itemscope itemtype="http://schema.org/WebSite">
 
@@ -31,25 +30,27 @@ $certificates_query = new WP_Query([
         </div>
 
         <?php if ($certificates_query->have_posts()): ?>
-        <div class="certificates-list">
-          <?php while ($certificates_query->have_posts()): ?>
-          <?php $certificates_query->the_post(); ?>
-          <article class="certificates-card">
-            <?php if ($thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'full')): ?>
-            <div class="certificates-card__image">
-              <img src="<?php echo $thumbnail; ?>" />
-            </div>
-            <a href="<?php echo $thumbnail; ?>" class="certificates-card__link" data-fslightbox="gallery" target="_blank">
-              <span class="certificates-card__link-icon">
-                <span class="icon icon-search"></span>
-              </span>
-            </a>
-            <?php endif; ?>
-            <div class="certificates-card__title"><?php echo esc_html(get_the_title()); ?></div>
-          </article>
-          <?php endwhile; ?>
-          <?php wp_reset_postdata(); ?>
-        </div>
+        <section class="certificates">
+          <div class="certificates-list">
+            <?php while ($certificates_query->have_posts()): ?>
+            <?php $certificates_query->the_post(); ?>
+            <article class="certificates-card">
+              <?php if ($thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'full')): ?>
+              <div class="certificates-card__image">
+                <img src="<?php echo $thumbnail; ?>" />
+              </div>
+              <a href="<?php echo $thumbnail; ?>" class="certificates-card__link" data-fslightbox="gallery" target="_blank">
+                <span class="certificates-card__link-icon">
+                  <span class="icon icon-search"></span>
+                </span>
+              </a>
+              <?php endif; ?>
+              <div class="certificates-card__title"><?php echo esc_html(get_the_title()); ?></div>
+            </article>
+            <?php endwhile; ?>
+            <?php wp_reset_postdata(); ?>
+          </div>
+        </section>
         <?php echo get_pagination($certificates_query); ?>
         <?php endif; ?>
       </div>
