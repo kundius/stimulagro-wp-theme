49523287c1a7eecde00d8a3913e6b686b6f31abf
Kundius Ruslan <kundius.ruslan@yandex.ru>
2025-12-29 13:41:22 +0300

    upd

--- templates/product.php
+++ templates/product.php
@@ -266,7 +266,7 @@ Template Name: Продукт
     </div>
 
     <div class="bgs3">
-      <div class="container">
+      <div class="container flex flex-col gap-16">
         <?php get_template_part('partials/delivery', null, [
           'anchor' => carbon_get_theme_option('crb_delivery_anchor'),
         ]); ?>
